c674d430bc7da6e0f55dfdd76fc0b3a803564af6
Kevin Buhler <kevinbuhlerr@gmail.com>
2020-11-02 19:57:56 -0700

    fdsaf

--- server/server.js
+++ server/server.js
@@ -188,6 +188,4 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+app.listen(process.env.PORT || 5000)
