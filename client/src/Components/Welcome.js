3395cbf7a2e60e3a6965cdb9c756caba5c90c091
Kevin Buhler <kevinbuhlerr@gmail.com>
2020-11-07 12:59:09 -0700

    potential fix for top link

--- client/src/Components/Welcome.js
+++ client/src/Components/Welcome.js
@@ -10,9 +10,9 @@ function HomeScreen() {
           <h2 className="homescreen__logoInfo"> #1 Rated Custom LED Lights</h2>
         </div>
         <div className="homescreen__button">
-          <a href="/shop" className="homescreen__buttonField">
+          <Link to="/shop" className="homescreen__buttonField">
             SHOP NOW
-          </a>
+          </Link>
         </div>
       </div>
     </section>
