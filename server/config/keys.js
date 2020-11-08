4b3065152e5bf195379e684c8554905e3a53b8bb
Kevin Buhler <kevinbuhlerr@gmail.com>
2020-11-07 18:45:29 -0700

    redo

--- server/config/keys.js
+++ server/config/keys.js
@@ -2,4 +2,4 @@ module.exports = {
   MongoURI: 'mongodb+srv://kevinbuhlerr:0vrdADLjTBKuL7BP@findnue1.xj8t2.mongodb.net/<dbname>?retryWrites=true&w=majority'
 }
 
-// test to see if works ahsf fdasffasdfsdjdls klsjdf lkdsajl bhkjbkjbk hjk  fsdaffsadf
+// test to see fsadfsadfjkasdfjdls klsjdf lkdsajl bhkjbkjbk hjk  fsdaffsadf
